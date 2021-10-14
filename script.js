08c8da387821019bf4db78dfbb550c22aa175d25
David Silveira <daviewave@gmail.com>
2021-10-14 17:59:52 -0400

    added variables that will randomly determine the character type and the character for each digit in the new password

--- script.js
+++ script.js
@@ -147,13 +147,14 @@ function getCriteria() {
     "*",
   ];
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
   const characterTypes = [
     lowerAlphabet,
     upperAlphabet,
     specialCharacter,
     numbers,
   ];
+
+  var randomCharacterType, randomCharacter;
 }
 
 /*Added Code*/
